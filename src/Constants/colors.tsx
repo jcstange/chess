13da1f149c70e927dd11c65bbd1137127a6bd9cc
jcstange <jcstange@gmail.com>
2021-07-26 17:29:23 +0300

    Add Colors

---
+++ src/Constants/colors.tsx
@@ -0,0 +1,8 @@
+export const Colors = {
+    black: '#000000',
+    white: '#FFFFFF',
+    light_brown: '#FFD8B0',
+    brown: '#C78D52',
+    selected_green: '#6dbfa1',
+    move_blue: '#68abba'
+}
